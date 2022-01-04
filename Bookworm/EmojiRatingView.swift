cf9df2b8fad7da3497f34be8c1b1441c1bb71bd9
Brandon Glenn <Brandon.Glenn@icloud.com>
2022-01-04 14:14:18 -0700

    Project 11 complete

---
+++ Bookworm/EmojiRatingView.swift
@@ -0,0 +1,37 @@
+//
+//  EmojiRatingView.swift
+//  Bookworm
+//
+//  Created by Brandon Glenn on 1/4/22.
+//
+
+import SwiftUI
+
+struct EmojiRatingView: View {
+    let rating: Int16
+    
+    
+    var body: some View {
+        switch rating {
+        case 1:
+            return Text("😡")
+        case 2:
+            return Text("😒")
+        case 3:
+            return Text("🤨")
+        case 4:
+            return Text("🤗")
+        case 5:
+            return Text("🤩")
+        default:
+            return Text("")
+        }
+        
+    }
+}
+
+struct EmojiRatingView_Previews: PreviewProvider {
+    static var previews: some View {
+        EmojiRatingView(rating: 3)
+    }
+}
